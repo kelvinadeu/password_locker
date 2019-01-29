6ec7fcb7008b0a6729d9e977fbeaf0c6d552050b
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-29 11:08:55 +0300

    add changes

--- password_locker.py
+++ password_locker.py
@@ -7,16 +7,18 @@ class password:
     password=[] #list to store user  passwords
 
     def_init_(self,account,username,password)
-
-      """
-      This function will allow the user to create instances of the class with unique details in each instance
-      """
-      self.account=account
-      self.username = username
-      self.password = password
+        """
+        This function will allow the user to create instances of the class with unique details in each instance
+        """
+        self.account=account
+        self.username = username
+        self.password = password
 
     def save_password(self):
+        """
+        This function will add users password to the password array
+        """
+        password.passwords.append(self)
 
-      """
-      This function will add users password to the password array
-      """
+    def generate_pass(length)
+        """
