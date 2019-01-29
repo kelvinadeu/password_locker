ecfd027dd1c1dcbd6adf3dfe92c2c0025d2e8c6f
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-29 23:59:20 +0300

    add changes

--- password_test.py
+++ password_test.py
@@ -24,4 +24,37 @@ class TestPassword(unittest.TestCase):
         self.assertEquall(self.new_password.account,"slack")
         self.assertEquall(self.new_password.username,"Adeu")
         self.assertEquall(self.new_password.password,"0713")
-        
+
+    def test_save_new_password(self):
+        """
+        Here it will check weather the new password is added in the list
+        """
+        self.new_password.save_password()
+        self.assertEquall(len(password.paswords),1)
+
+    def test_add_generate_password(self):
+        """
+        This will check if the new password added to the list
+        """
+        new_password = password("facebook","6980",Pasword.generate_(6))
+        new_password.save_password()
+        self.assertEquall(len(Password.password)1)
+
+    def test_display_password(self)
+        """
+        Here it checks weather the display_Password function will return the password in the password list
+        """
+        self.new_password.test_save_new_password()
+        new_pass = Password("facebook","6980",Password.generate_pass(6))
+        new_pass.save_pass()
+        self.assertEqual(len(Password.passwords),len(Password.display_passwords()))
+
+    def test_delete(self):
+        """
+        This test will check whether the password gets deleted from the passwords list
+        """
+        self.new_password.save_password()
+        new_password = Password("facebook","6980",Password.generate_password(8))
+        new_password.save_password()
+        Password.delete_password("instagram")
+        self.assertEquall(len(password.password),1)
