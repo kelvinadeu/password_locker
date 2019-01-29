d239c9a9cb2c3bd1beeb2690fb82608e57f75413
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-29 06:29:11 +0300

    add changes

--- password_locker.py
+++ password_locker.py
@@ -6,4 +6,10 @@ class password:
     """
     password=[]
     def_init_(self,account,username,password):
-    
+      """
+      self.account=account
+      self.username = username
+      self.password = password
+
+    def save_password(self):
+      """  
