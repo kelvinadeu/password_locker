6ac27a46a92d2e8c5aa9efa8a4c5f677732c1f92
kelvinadeu <kelvinadeu25@gmail.com>
2019-01-29 15:22:59 +0300

    add changes

---
+++ user.py
@@ -0,0 +1,12 @@
+class user:
+    """
+    This class will contain all details of the user
+    """
+    def_init_(self,login,password):
+      """
+      This will create unique details for each of the user class
+      """
+      self.login = login
+      self.password = password
+
+    def_exist(self,password): 
